4f096d970bbfbbd5d931543e7bf41701fdea2554
MrCooper42 <mr.cooper42co@gmail.com>
2021-07-08 19:01:49 -0600

    refactor to use context for logged in state

--- sideEfectsAndReducers/src/components/Login/Login.js
+++ sideEfectsAndReducers/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, {useEffect, useReducer, useState} from "react";
+import React, {useContext, useEffect, useReducer, useState} from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../../store/auth-context";
 
 const emailReducer = (state, action) => {
 	if (action.type === "USER_INPUT") {
@@ -42,8 +43,10 @@ const passwordReducer = (state, action) => {
 	};
 };
 
-const Login = (props) => {
+const Login = () => {
 	const [formIsValid, setFormIsValid] = useState(false);
+
+	const context = useContext(AuthContext);
 
 	const [
 		emailState,
@@ -113,7 +116,7 @@ const Login = (props) => {
 
 	const submitHandler = (event) => {
 		event.preventDefault();
-		props.onLogin(emailState.value, passwordState.value);
+		context.onLogin(emailState.value, passwordState.value);
 	};
 
 	return (
